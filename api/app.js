5d0c9a29f13f1949939b85419ced9864bdbd46e3
Facundo Garcia Brunetti <122062065+facundobrunetti99@users.noreply.github.com>
2025-08-14 11:05:25 -0300

    finalcommit

--- api/app.js
+++ api/app.js
@@ -9,7 +9,6 @@ import storyRoutes from "../routes/story.router.js";
 import cors from "cors";
 
 const app = express();
-// Configuración de CORS más robusta
 app.use(cors({
   origin: true, 
   credentials: true,
@@ -19,7 +18,6 @@ app.use(cors({
 }));
 
 
-// Middleware para headers CORS manuales (backup)
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://front-end-task-pied.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
