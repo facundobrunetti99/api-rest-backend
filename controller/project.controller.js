25370cc9a4cb1d4939b7cf9d6bf74cf07eb0edea
Facundo Garcia Brunetti <122062065+facundobrunetti99@users.noreply.github.com>
2025-08-14 10:22:17 -0300

    commitfinal

--- controller/project.controller.js
+++ controller/project.controller.js
@@ -1,5 +1,7 @@
 import Project from "../model/project.model.js";
-
+import Epic from "../model/epic.model.js";
+import Story from "../model/story.model.js";
+import Task from "../model/task.model.js";
 export const createProject = async (req, res) => {
   try {
     const { title, description, date } = req.body;
@@ -39,27 +41,35 @@ export const updateProject = async (req, res) => {
     const updatedProject = await req.project.save();
     res.json(updatedProject);
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message: "Error al actualizar el proyecto",
-        error: error.message,
-      });
+    res.status(500).json({
+      message: "Error al actualizar el proyecto",
+      error: error.message,
+    });
   }
 };
 
 export const deleteProject = async (req, res) => {
   try {
     const projectId = req.project._id;
-    await Task.deleteMany({ project: projectId });
-    await Story.deleteMany({ project: projectId });
-    await Epic.deleteMany({ project: projectId });
+
+    // 1. Obtener todos los epics del proyecto
+    const epics = await Epic.find({ project: projectId });
+    const epicIds = epics.map((epic) => epic._id);
+    //obtener todas las stories de esos epics
+    const stories = await Story.find({ epic: { $in: epicIds } });
+    const storyIds = stories.map((story) => story._id);
+    //eliminar todas las tasks de esas stories
+    const deletedTasks = await Task.deleteMany({ story: { $in: storyIds } });
+    //eliminar todas las stories de esos epics
+    const deletedStories = await Story.deleteMany({ epic: { $in: epicIds } });
+    //eliminar todos los epics del proyecto
+    const deletedEpics = await Epic.deleteMany({ project: projectId });
+    //eliminar el proyecto
     await req.project.deleteOne();
-    res.json({ message: "Proyecto eliminado" });
+    res.json({ message: "Proyecto eliminado correctamente" });
   } catch (error) {
     res
       .status(500)
       .json({ message: "Error al eliminar el proyecto", error: error.message });
   }
 };
-
